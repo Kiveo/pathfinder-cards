b629070dfbdb664639ef152d61e9858d162f309e
Mav <mavericktesini@gmail.com>
2019-11-26 01:38:14 -0500

    Add dummy samples for useState and useEffect to affirm eslint settings

--- pf2e-cards-ui/src/App.js
+++ pf2e-cards-ui/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
-);
+const App = () => {
+  // -- lifecycles and hooks --
+  const [sample, setSample] = useState(0);
+  const [next, setNext] = useState(0);
+  useEffect(() => {
+    setNext(sample + 1);
+  }, [sample]);
+
+  // -- RENDER --
+  return (
+    <div>
+      <button type="button" onClick={() => setSample(sample + 1)}>
+        Increment Hook State
+      </button>
+      <hr />
+      Sample:
+      {sample}
+      <br />
+      {`Next: ${next}`}
+    </div>
+  );
+};
 
 export default App;
