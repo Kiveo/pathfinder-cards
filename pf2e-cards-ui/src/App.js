fbca93c26c0ccac85b74e1b8136a378abec05ccf
Mav <mavericktesini@gmail.com>
2019-11-26 01:27:41 -0500

    Install and configure react with eslint airbnb and custom rules

--- pf2e-cards-ui/src/App.js
+++ pf2e-cards-ui/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    Hello World
+  </div>
+);
 
 export default App;
