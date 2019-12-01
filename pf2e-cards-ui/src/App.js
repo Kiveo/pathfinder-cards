47c55cdad4011493930aa022fa5a585fd5449a1c
Mav <mavericktesini@gmail.com>
2019-11-30 23:55:25 -0500

    Leave todo for later dev of App.js

--- pf2e-cards-ui/src/App.js
+++ pf2e-cards-ui/src/App.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Page, PageHeading } from 'components/core';
 import UserContext from 'UserContext';
 
-// TODO: demo react context hook
+// TODO: demo react context hook + useReducer as smallscale alt to Redux
 const App = () => {
   const value = useContext(UserContext);
 
