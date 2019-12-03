5edad305c5bfc180507b6641504a5f9b4cfa8322
Mav <mavericktesini@gmail.com>
2019-12-03 16:07:33 -0500

    remove clutter in server.js

--- pf2e-mw/server.js
+++ pf2e-mw/server.js
@@ -5,11 +5,6 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
-//Static file declarationapp.use(express.static(path.join(__dirname, 'pf2e-cards-ui/build')));
-//production modeif(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'pf2e-cards-ui/build')));  
-//  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'pf2e-cards-ui/build/index.html'));  })}
-//build modeapp.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/pf2e-cards-ui/public/index.html'));})
-//start serverapp.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
 
 mongoose.connect(process.env.DATABASE_URL || MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const db = mongoose.connection;
