177572b54ffccc35652f02b70a480d46f0e8a9ed
Mav <mavericktesini@gmail.com>
2019-12-03 17:43:54 -0500

    adjust server for production app.use

--- pf2e-mw/server.js
+++ pf2e-mw/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds351628.mlab.com:51628/heroku_0ckwtrmz", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
@@ -14,7 +14,7 @@ db.once('open', () => console.log("Connection successful"))
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static('pf2e-cards-ui/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join('build', 'index.html'));
   });
