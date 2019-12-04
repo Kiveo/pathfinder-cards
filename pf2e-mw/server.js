7c4e4c41c6d23c20e81c17fa0a2fb6ca1810ec77
Mav <mavericktesini@gmail.com>
2019-12-03 22:16:18 -0500

    shuffle api and production catcher server.js

--- pf2e-mw/server.js
+++ pf2e-mw/server.js
@@ -15,6 +15,13 @@ db.once('open', () => console.log("Connection successful"))
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('pf2e-cards-ui/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('pf2e-cards-ui', 'build', 'index.html'));
+  });
+}
+
 // routes
 const cardsRouter = require('./routes/cards');
 app.use('/cards', cardsRouter);
@@ -22,11 +29,5 @@ app.use('/', (req, res, next) => {
   res.redirect('/cards')
 })
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('pf2e-cards-ui/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('pf2e-cards-ui', 'build', 'index.html'));
-  });
-}
 
 app.listen(port, () => console.log("Server listening..."));
